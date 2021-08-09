c47ac1f6c60cc97ad0efa730b90376e3907f629a
Ankor-sys <sebasmoreira4@gmail.com>
2021-08-08 21:32:25 -0600

    Inserts de Pacientes en BD

--- BD_Desindexado.sql
+++ BD_Desindexado.sql
@@ -8,7 +8,7 @@ use CLINICA1;
 #GRANT ALL PRIVILEGES ON clinica TO 'root'@'localhost' IDENTIFIED BY 'Sebas1234' ;
 
 create table TIPO_USUARIO(
-id_tipo_usuario varchar(15) primary key,
+id_tipo_usuario varchar(15) primary key not null,
 nombre_tipo_usuario varchar(30) not null
 
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -16,7 +16,7 @@ nombre_tipo_usuario varchar(30) not null
 insert into tipo_usuario values ("1", "Admin");
 
 create table USUARIOS(
-id_usuario varchar(15) primary key,
+id_usuario varchar(15) primary key not null,
 id_tipo_usuario varchar(15) not null,
 nombre_usuario varchar(30) not null,
 passwd_usuario varchar(15) not null,
@@ -31,7 +31,7 @@ select * from usuarios;
 #FORMAS DE PAGO Y RELACIONADOS
 
 create table MONEDA(
-id_moneda varchar(15) primary key,
+id_moneda varchar(15) primary key not null,
 nombre_moneda varchar(30) not null,
 tipo_cambio float not null,
 status_moneda varchar(1)
@@ -57,12 +57,12 @@ foreign key (id_moneda) references MONEDA(id_moneda)
 */
 
 create table ASEGURADORAS(
-id_aseguradora varchar(15) primary key,
+id_aseguradora varchar(15) primary key not null,
 nombre_aseguradora varchar(30) not null
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table DESCUENTOS_ASEGURADORAS(
-id_descuento varchar(15) primary key,
+id_descuento varchar(15) primary key not null,
 nombre_descuento varchar(30) not null,
 descuento float not null,
 id_aseguradora varchar(15),
@@ -72,7 +72,7 @@ foreign key (id_aseguradora) references ASEGURADORAS(id_aseguradora)
 
 #GESTION CLINICA Y ADMINISTRATIVA
 create table BITACORA(
-id_bitacora varchar(15) primary key,
+id_bitacora varchar(15) primary key not null,
 id_usuario varchar(15) not null,
 descripcion_bitacora varchar(50),
 fecha_bitacora date not null,
@@ -94,7 +94,7 @@ insert into sede values ('2', 'VillaNueva', 'Villa Nueva', '25262422');
 insert into sede values ('3', 'SanKris', 'San Cristobal', '25262422');
 
 create table PUESTO(
-id_puesto varchar(15) primary key,
+id_puesto varchar(15) primary key not null,
 nombre_puesto varchar(15) not null,
 status_puesto varchar(1)
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -121,11 +121,13 @@ seguro_paciente varchar(30) not null,
 primary key (id_paciente, nit_paciente)
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
+insert into clinica1.paciente values ('1', '3003999650101', '2018658-4', 'Meredid', 'Blake', 'F', '21', '58595653', 'Ciudad', 'mblake@gmail.com', '1', '789');
+insert into clinica1.paciente values ('2', '3003997898655', '2089698-7', 'Mario', 'Bros', 'M', '29', '5963565', 'Ciudad', 'mbros@gmail.com', '1', '7895');
+insert into clinica1.paciente values ('3', '3004856582254', '2896994-8', 'Bad', 'Bunny', 'M', '33', '47859912', 'Ciudad', 'benito@gmail.com', '1', '653');
 select * from paciente;	
 
 create table EMPLEADO(
-id_empleado varchar(15) primary key,
+id_empleado varchar(15) primary key not null,
 cui_empleado varchar(30) not null,
 nit_empleado varchar(10) not null,
 nombre_empleado varchar(30) not null,
@@ -150,7 +152,7 @@ genero_empleado, edad_empleado, telefono_empleado, direccion_empleado, email_emp
 select * from empleado;
 
 create table REQUERIMIENTOS_CLINICOS(
-id_requerimiento_clinico varchar(15) primary key,
+id_requerimiento_clinico varchar(15) primary key not null,
 descripcion_requerimiento_clinico varchar(100) not null, 
 cantidad int not null
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -158,19 +160,19 @@ cantidad int not null
 select * from REQUERIMIENTOS_CLINICOS;
 
 create table REQUERIMIENTOS_PACIENTE(
-id_requerimiento_paciente varchar(15) primary key,
+id_requerimiento_paciente varchar(15) primary key not null,
 descripcion_requerimiento_paciente varchar(100) not null
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 select * from REQUERIMIENTOS_PACIENTE;
 
 create table TIPO_EXAMEN(
-id_tipo_examen varchar(15) primary key,
+id_tipo_examen varchar(15) primary key not null,
 nombre_tipo_examen varchar(30) not null
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table EXAMEN(
-id_examen varchar(15) primary key,
+id_examen varchar(15) primary key not null,
 nombre_examen varchar(30) not null,
 id_tipo_examen varchar(15) not null,
 id_requerimiento_clinico varchar(15) not null,
@@ -183,12 +185,12 @@ foreign key (id_requerimiento_paciente) references REQUERIMIENTOS_PACIENTE(id_re
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table PAQUETE_ENCABEZADO(
-id_paquete varchar(15) primary key,
+id_paquete varchar(15) primary key not null,
 nombre_paquete varchar(30) not null
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table PAQUETE_DETALLE(
-id_detalle varchar(15) primary key,
+id_detalle varchar(15) primary key not null,
 id_paquete varchar(15) not null,
 id_examen varchar(30) not null,
 
@@ -197,11 +199,11 @@ foreign key (id_examen) references EXAMEN(id_examen)
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table CITAS(
-id_cita varchar(15) primary key,
+id_cita varchar(15) primary key not null,
 id_paciente varchar(15) not null,
 id_encargado varchar(15) not null,
 fecha_cita date not null,
-hora_cita time not null,
+descripcion varchar(150) not null,
 status_cita varchar(1) not null,
 
 foreign key (id_paciente) references PACIENTE(id_paciente),
@@ -209,12 +211,12 @@ foreign key (id_encargado) references EMPLEADO(id_empleado)
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table TIPO_MUESTRA(
-id_tipo_muestra varchar(15) primary key,
+id_tipo_muestra varchar(15) primary key not null,
 nombre_tipo_muestra varchar(30) not null
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table MUESTRAS(
-id_muestra varchar(15) primary key,
+id_muestra varchar(15) primary key not null,
 id_paciente varchar(15) not null,
 id_tipo_muestra varchar(15) not null,
 fecha_muestra date not null,
@@ -225,7 +227,7 @@ foreign key (id_tipo_muestra) references TIPO_MUESTRA(id_tipo_muestra)
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table ETIQUETAS(
-id_etiqueta varchar(15) primary key,
+id_etiqueta varchar(15) primary key not null,
 id_muestra varchar(15) not null,
 nombre_paciente varchar(30) not null,
 tipo_muestra varchar(15) not null,
@@ -234,7 +236,7 @@ foreign key (id_muestra) references MUESTRAS(id_muestra)
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table ENCABEZADO_FACTURA(
-id_encabezado_factura varchar(15) primary key,
+id_encabezado_factura varchar(15) primary key not null,
 id_paciente varchar(15) not null,
 id_empleado varchar(15) not null,
 fecha_factura date not null,
@@ -245,7 +247,7 @@ foreign key (id_empleado) references EMPLEADO(id_empleado)
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table DETALLE_FACTURA(
-id_detalle_factura varchar(15) primary key,
+id_detalle_factura varchar(15) primary key not null,
 id_encabezado_factura varchar(15) not null,
 descripcion varchar(15) not null,
 total_factura float not null,
@@ -255,14 +257,14 @@ foreign key (id_encabezado_factura) references ENCABEZADO_FACTURA(id_encabezado_
 
 
 create table ENCABEZADO_EXPEDIENTE(
-id_encabezado_expediente varchar(15) primary key,
+id_encabezado_expediente varchar(15) primary key not null,
 id_paciente varchar(15) not null,
 
 foreign key (id_paciente) references PACIENTE(id_paciente)
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table DETALLE_EXPEDIENTE(
-id_detalle_expediente varchar(15) primary key,
+id_detalle_expediente varchar(15) primary key not null,
 id_encabezado_expediente varchar(15) not null,
 fecha_expediente date not null,
 hora_expediente time not null,
@@ -273,7 +275,7 @@ foreign key (descripcion_resultados) references DETALLE_FACTURA(id_detalle_factu
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table ENCABEZADO_COTIZACION(
-id_encabezado_cotizacion varchar(15) primary key,
+id_encabezado_cotizacion varchar(15) primary key not null,
 id_paciente varchar(15) not null,
 id_empleado varchar(15) not null,
 fecha_cotizacion date not null,
@@ -286,7 +288,7 @@ foreign key (id_empleado) references EMPLEADO(id_empleado)
 #eliminar carrito y poner solo detalle cotizacion
 
 create table DETALLE_COTIZACION(
-id_detalle_cotizacion varchar(15) primary key,
+id_detalle_cotizacion varchar(15) primary key not null,
 id_encabezado_cotizacion varchar(15) not null,
 id_paquete varchar(15),
 id_examen varchar(15) null,
@@ -298,7 +300,7 @@ foreign key (id_paquete) references PAQUETE_ENCABEZADO(id_paquete)
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table CAJA(
-id_caja varchar(15) primary key,
+id_caja varchar(15) primary key not null,
 nombre_caja varchar(30) not null,
 saldo_caja float not null,
 id_empleado varchar(30) not null,
@@ -312,13 +314,13 @@ foreign key (id_empleado) references EMPLEADO(id_empleado)
 
 #INDICES
 create table FORMAS_PAGO(
-id_forma_pago varchar(15) primary key,
+id_forma_pago varchar(15) primary key not null,
 nombre_forma_pago varchar(30) not null,
 status_forma_pago varchar(1) not null
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table HISTORIAL_PAGOS(
-id_historial varchar(15) primary key,
+id_historial varchar(15) primary key not null,
 id_paciente varchar(15) not null,
 id_forma_pago varchar(15) not null,
 id_fecha date not null,
@@ -330,7 +332,7 @@ foreign key (id_forma_pago) references FORMAS_PAGO(id_forma_pago)
 )ENGINE=InnoDB DEFAULT CHARSET =utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 create table CREDITO(
-id_credito varchar(15) primary key,
+id_credito varchar(15) primary key not null,
 id_paciente varchar(15) not null,
 id_forma_pago varchar(15) not null,
 id_factura varchar(15) not null,
